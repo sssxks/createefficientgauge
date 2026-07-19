cf8369ceaaa725b40ab3340038eb10074b440edc
xks <2777389616@qq.com>
2026-07-19 14:12:34 +0800

    feat: animate the handheld cannon model

---
+++ mods/create-handheld-cannon/src/main/java/io/github/createhandheldcannon/client/HandheldCannonItemRenderer.java
@@ -0,0 +1,36 @@
+package io.github.createhandheldcannon.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.foundation.item.render.CustomRenderedItemModel;
+import com.simibubi.create.foundation.item.render.CustomRenderedItemModelRenderer;
+import com.simibubi.create.foundation.item.render.PartialItemModelRenderer;
+
+import dev.engine_room.flywheel.lib.model.baked.PartialModel;
+import dev.engine_room.flywheel.lib.transform.TransformStack;
+import io.github.createhandheldcannon.CreateHandheldCannon;
+import net.createmod.catnip.animation.AnimationTickHolder;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
+
+public final class HandheldCannonItemRenderer extends CustomRenderedItemModelRenderer {
+    private static final PartialModel COG =
+        PartialModel.of(CreateHandheldCannon.id("item/handheld_cannon/cog"));
+    private static final Vec3 COG_CENTER = new Vec3(10.5 / 16f, 9.5 / 16f, 2.8 / 16f);
+
+    @Override
+    protected void render(ItemStack stack, CustomRenderedItemModel model, PartialItemModelRenderer renderer,
+        ItemDisplayContext transformType, PoseStack pose, MultiBufferSource buffer, int light, int overlay) {
+        renderer.renderSolid(model.getOriginalModel(), light);
+
+        pose.pushPose();
+        float angle = (AnimationTickHolder.getRenderTime() * -2.5f) % 360;
+        TransformStack.of(pose)
+            .translate(COG_CENTER)
+            .rotateZDegrees(angle)
+            .translateBack(COG_CENTER);
+        renderer.renderSolid(COG.get(), light);
+        pose.popPose();
+    }
+}
